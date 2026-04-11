b86b9a3a4e10a666a3a4907583b17e3fc702fcd3
Kilikaev Victor <kv2013@gmail.com>
2026-04-11 18:02:03 +0500

    улутшение: добавляем мьютекс для безопасного доступа к данным

--- internal/repository/inmemory/inmemory.go
+++ internal/repository/inmemory/inmemory.go
@@ -2,6 +2,7 @@ package inmemory
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/KV2013/url-shortner-go/internal/model"
 )
@@ -9,6 +10,7 @@ import (
 var ErrIDAlreadyExists = errors.New("id уже занят")
 
 type InMemoryRepository struct {
+	mu       sync.RWMutex
 	UrlsByID map[string]model.URL
 }
 
@@ -19,6 +21,9 @@ func NewRepository() (*InMemoryRepository, error) {
 }
 
 func (r *InMemoryRepository) GetByID(id string) (*model.URL, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	found, exists := r.UrlsByID[id]
 	if !exists {
 		return nil, false
@@ -28,6 +33,9 @@ func (r *InMemoryRepository) GetByID(id string) (*model.URL, bool) {
 }
 
 func (r *InMemoryRepository) Save(url *model.URL) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	if _, exists := r.UrlsByID[url.Short]; exists {
 		return ErrIDAlreadyExists
 	}
