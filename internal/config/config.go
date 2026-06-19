f583ce9381818fe67ae49c021ec08440a37e1b42
victor <kv2013@gmail.com>
2026-06-19 19:04:12 +0500

    исправление: gofmt

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,24 +3,24 @@ package config
 import "github.com/caarlos0/env/v6"
 
 type Config struct {
-	ServerAddress       string `env:"SERVER_ADDRESS"`
-	BaseURL             string `env:"BASE_URL"`
-	LogLevel            string `env:"LOG_LEVEL"`
-	FileStoragePath     string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN         string `env:"DATABASE_DSN"`
-	JWTSecretKey        string `env:"JWT_SECRET_KEY"`
-	AuditFile           string `env:"AUDIT_FILE"`
-	AuditURL            string `env:"AUDIT_URL"`
-	EnablePprof         bool   `env:"ENABLE_PPROF"`
-	AuditMaxConcurrent  int    `env:"AUDIT_MAX_CONCURRENT"`
+	ServerAddress      string `env:"SERVER_ADDRESS"`
+	BaseURL            string `env:"BASE_URL"`
+	LogLevel           string `env:"LOG_LEVEL"`
+	FileStoragePath    string `env:"FILE_STORAGE_PATH"`
+	DatabaseDSN        string `env:"DATABASE_DSN"`
+	JWTSecretKey       string `env:"JWT_SECRET_KEY"`
+	AuditFile          string `env:"AUDIT_FILE"`
+	AuditURL           string `env:"AUDIT_URL"`
+	EnablePprof        bool   `env:"ENABLE_PPROF"`
+	AuditMaxConcurrent int    `env:"AUDIT_MAX_CONCURRENT"`
 }
 
 func NewConfig() (*Config, error) {
 
 	cfg := Config{
-		ServerAddress: "localhost:8080",
-		BaseURL:       "http://localhost:8080",
-		LogLevel:      "info",
+		ServerAddress:      "localhost:8080",
+		BaseURL:            "http://localhost:8080",
+		LogLevel:           "info",
 		JWTSecretKey:       "default-secret-change-me",
 		AuditMaxConcurrent: 10,
 	}
